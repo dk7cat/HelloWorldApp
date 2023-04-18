f45599537bab06488f9e1c20da424e0bb1de63e3
Denis Koshkarev <dk7cat@icloud.com>
2023-04-19 00:21:51 +0800

    Setup main storyboard and vc

--- HelloWorldApp/ViewController.swift
+++ HelloWorldApp/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var greetingLabel: UILabel!
+    @IBOutlet var greetingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        greetingLabel.isHidden = true
+        greetingButton.configuration = setupButton(with: "Show Greeting")
     }
 
-
+    @IBAction func greetingButtonPressed() {
+        greetingLabel.isHidden.toggle()
+        greetingButton.configuration = setupButton(
+            with: greetingLabel.isHidden ? "Show Greeting" : "Hide Greeting")
+    }
+    
+    private func setupButton(with title: String) -> UIButton.Configuration {
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = #colorLiteral(red: 0.9411764741, green: 0.4980392158, blue: 0.3529411852, alpha: 1)
+        buttonConfiguration.title = title
+        buttonConfiguration.buttonSize = .large
+        buttonConfiguration.attributedTitle?.font = UIFont.systemFont(ofSize: 24)
+        return buttonConfiguration
+    }
+    
 }
 
